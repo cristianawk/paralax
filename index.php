35a411e711e1d8841cb39302ddcbc5ad24d55f08
Cristian <cristianms.awk@gmail.com>
2018-07-20 00:50:49 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<title>Efeito Paralax | CSS Puro | JavaScript</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <header>
+        <h1>Efeito Paralax</h1>
+        <h2>Css Puro</h2>
+    </header>
+
+    <div id="corpo">
+        <p>
+            <h1>El pasaje estándar Lorem Ipsum, usado desde el año 1500.</h1>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+            <h1>Sección 1.10.32 de "de Finibus Bonorum et Malorum", escrito por Cicero en el 45 antes de Cristo</h1>
+            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+        </p>
+    </div>
+
+	<footer>
+        <p>Copyright 2018 - by Cristian Marques
+        <br/>
+        <a href="http://www.facebook.com/cristianawk" target="blank">Facebook </a>| <a href="http://twitter.com/cristianawk" target="blank">Twitter </a></p>
+    </footer>
+
+</body>
+</html>
